a16365df8fe86d7e007a70ce76f480f6d144c5e7
DionRBLX <zandro.van.hecke@outlook.com>
2017-04-01 16:52:40 +0200

    Update main.cpp

--- main/main.cpp
+++ main/main.cpp
@@ -11,31 +11,6 @@
 using namespace std;
 
 
-//FUNCTIONS
-DWORD GetModuleBaseAddress(DWORD dwProcessIdentifier, TCHAR *lpszModuleName)
-{
-    HANDLE hSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE, dwProcessIdentifier);
-    DWORD dwModuleBaseAddress = 0;
-    if (hSnapshot != INVALID_HANDLE_VALUE)
-    {
-        MODULEENTRY32 ModuleEntry32 = { 0 };
-        ModuleEntry32.dwSize = sizeof(MODULEENTRY32);
-        if (Module32First(hSnapshot, &ModuleEntry32))
-        {
-            do
-            {
-                if (_tcscmp(ModuleEntry32.szModule, lpszModuleName) == 0)
-                {
-                    dwModuleBaseAddress = (DWORD)ModuleEntry32.modBaseAddr;
-                    break;
-                }
-            } while (Module32Next(hSnapshot, &ModuleEntry32));
-        }
-        CloseHandle(hSnapshot);
-    }
-    return dwModuleBaseAddress;
-}
-
 
 int Main() {
 //Here is where the magic starts
@@ -50,7 +25,7 @@ while (hWnd == 0) { // Waiting until found
 
      system("cls");
      DWORD pId;
-     GetWindowThreadProcessId(hWnd, &pId);
+     custom::GetWindowThreadProcessId(hWnd, &pId);
      DWORD RBLXBASEAddr = GetModuleBaseAddress(pId, "RobloxPlayerBeta.exe"); 
      std::cout << "Base Address: " << hex << GetModuleBaseAddress(;
 
