8355eeb3e32ecd4aaec6b3bc0439c9dccd110c57
DionRBLX <zandro.van.hecke@outlook.com>
2017-04-01 16:52:20 +0200

    Update costumfunc.cpp

--- COSTUM/costumfunc.cpp
+++ COSTUM/costumfunc.cpp
@@ -7,8 +7,34 @@
 
 namespace costum
  {
+	//Costum func
     int sleepthread(int amountoftime) {
 		SetConsoleTitle("Thread is sleeping for " + amtTime + " ;*"); 
 	  Sleep(amountoftime);
 	  }
+	//Not a costum func
+	DWORD GetModuleBaseAddress(DWORD dwProcessIdentifier, TCHAR *lpszModuleName)
+{
+    HANDLE hSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE, dwProcessIdentifier);
+    DWORD dwModuleBaseAddress = 0;
+    if (hSnapshot != INVALID_HANDLE_VALUE)
+    {
+        MODULEENTRY32 ModuleEntry32 = { 0 };
+        ModuleEntry32.dwSize = sizeof(MODULEENTRY32);
+        if (Module32First(hSnapshot, &ModuleEntry32))
+        {
+            do
+            {
+                if (_tcscmp(ModuleEntry32.szModule, lpszModuleName) == 0)
+                {
+                    dwModuleBaseAddress = (DWORD)ModuleEntry32.modBaseAddr;
+                    break;
+                }
+            } while (Module32Next(hSnapshot, &ModuleEntry32));
+        }
+        CloseHandle(hSnapshot);
+    }
+    return dwModuleBaseAddress;
+}
+
   }
