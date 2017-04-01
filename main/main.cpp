0e0add04450f8889e21a22dd34e474227fb1bb61
DionRBLX <zandro.van.hecke@outlook.com>
2017-04-01 16:38:55 +0200

    Main coding begon
    Added Getmodulebase starting on the magic in the main

---
+++ main/main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <windows.h>
+#include <TlHelp32.h>
+#include <TCHAR.h>
+#include <string>
+#include <sstream>
+
+//Here is where the hax begin 
+
+using namespace std;
+
+
+//FUNCTIONS
+DWORD GetModuleBaseAddress(DWORD dwProcessIdentifier, TCHAR *lpszModuleName)
+{
+    HANDLE hSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE, dwProcessIdentifier);
+    DWORD dwModuleBaseAddress = 0;
+    if (hSnapshot != INVALID_HANDLE_VALUE)
+    {
+        MODULEENTRY32 ModuleEntry32 = { 0 };
+        ModuleEntry32.dwSize = sizeof(MODULEENTRY32);
+        if (Module32First(hSnapshot, &ModuleEntry32))
+        {
+            do
+            {
+                if (_tcscmp(ModuleEntry32.szModule, lpszModuleName) == 0)
+                {
+                    dwModuleBaseAddress = (DWORD)ModuleEntry32.modBaseAddr;
+                    break;
+                }
+            } while (Module32Next(hSnapshot, &ModuleEntry32));
+        }
+        CloseHandle(hSnapshot);
+    }
+    return dwModuleBaseAddress;
+}
+
+int Main() {
+//Here is where the magic starts
+
+}
+
+ 
